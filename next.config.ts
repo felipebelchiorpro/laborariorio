037d22c4d22c51b817efd29f30c5e6f9bee466a9
Felipe Belchior <contatofelipebelchior@gmail.com>
2025-09-09 23:13:41 +0000

    a logo esta na pasta

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   // config options here
+  // Forcing a server restart to clear cache.
   typescript: {
     ignoreBuildErrors: true,
   },
