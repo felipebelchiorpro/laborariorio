ece4ddad99e11e47d12191ed0eb32e5d972b2e53
Felipe Belchior <contatofelipebelchior@gmail.com>
2025-09-09 23:15:44 +0000

    não foi ainda

--- src/middleware.ts
+++ src/middleware.ts
@@ -45,5 +45,15 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - public files (e.g. /logo-prefeitura.png) by using a negative lookahead
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|.*\\.png$).*)',
+  ],
 };
