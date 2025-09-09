e11695e1fcd9351a8483442d104b944b15f47712
Felipe Belchior <contatofelipebelchior@gmail.com>
2025-09-09 00:29:26 +0000

    deixe em portugues (_for element <Button>_)

--- src/components/dashboard/data-table-view-options.tsx
+++ src/components/dashboard/data-table-view-options.tsx
@@ -29,7 +29,7 @@ export function DataTableViewOptions<TData>({
           className="ml-auto hidden h-9 lg:flex"
         >
           <SlidersHorizontal className="mr-2 h-4 w-4" />
-          Visualizar
+          Colunas
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-[150px]">
