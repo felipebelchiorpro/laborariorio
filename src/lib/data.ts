4d4d4e79b8e1ff6386e989982ed52223275500b8
Felipe Belchior <contatofelipebelchior@gmail.com>
2025-09-12 00:10:31 +0000

    UBS SANTO ANTONIO, UBS VARZEA, UBS SÃO JOSÉ, UBS REDENTOR no caso os que

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -2,7 +2,9 @@
 export const withdrawnByOptions: { value: string; label: string }[] = [
     { value: "Municipal", label: "Municipal" },
     { value: "UBS REDENTOR", label: "UBS REDENTOR" },
+    { value: "UBS SANTO ANTONIO", label: "UBS SANTO ANTONIO" },
+    { value: "UBS VARZEA", label: "UBS VARZEA" },
+    { value: "UBS SÃO JOSÉ", label: "UBS SÃO JOSÉ" },
     { value: "RETIRADO", label: "RETIRADO" },
     { value: "CEAM", label: "CEAM" },
-    { value: "SANTO ANTONIO", label: "SANTO ANTONIO" },
 ];
